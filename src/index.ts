6a2a03c2f3e0bcd028db97e29cde0ce9e9016fbb
Sahilgggg <sahilgggfdcbu@gmail.com>
2026-03-29 17:47:37 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    console.log("HELLO ROUTE HIT");
+    res.send("THIS IS MY SERVER");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
