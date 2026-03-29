4ae74a0d4f521efabef0bf63dcf5245193ba2b3c
Sahilgggg <sahilgggfdcbu@gmail.com>
2026-03-29 22:01:31 +0530

    implement database schema

---
+++ src/db/index.ts
@@ -0,0 +1,10 @@
+
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+
+if(!process.env.DATABASE_URL) throw new Error("DATABASE is required");
+const sql = neon(process.env.DATABASE_URL);
+export const index = drizzle(sql);
