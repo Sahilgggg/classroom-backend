29cb07429e46a464f6d4ce9a10280f477d4bc90c
Sahilgggg <sahilgggfdcbu@gmail.com>
2026-03-30 01:53:41 +0530

    implement all subjects routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,20 @@
 import express from "express";
+import subjectsRouter from "./routes/subjects";
+import cors from "cors";
 const app = express();
 const PORT = 5000;
 
+app.use(
+    cors({
+        origin: process.env.FRONTEND_URL, // React app URL
+        methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
+        credentials: true, // allow cookies
+    })
+);
+
 app.use(express.json());
+
+app.use("/api/subjects", subjectsRouter);
 
 app.get("/", (req, res) => {
     console.log("HELLO ROUTE HIT");
